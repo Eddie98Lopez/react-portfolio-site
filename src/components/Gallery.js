8f32c063520f167eef3b11e0af5fc26b98a3c352
Eduardo Lopez <ed98lopez@gmail.com>
2021-12-23 00:07:49 -0800

    added begginings of new nav. My plan is to figure out layout of desktop before moving to mobile

---
+++ src/components/Gallery.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Gallery = () => {
+    return (
+        <div>
+            This will be a gallery
+        </div>
+    )
+}
+
+export default Gallery
--- src/components/styled-components/index.js
+++ src/components/styled-components/index.js
@@ -1,6 +1,6 @@
 import AppWrapper from "./AppWrapper";
 import Section from "./Section";
-import {Input as StyledInput,Label,TextArea, Form} from './Forms'
+import { Input as StyledInput, Label, TextArea, Form } from "./Forms";
+import { Nav, NavLinks } from "./Navigation";
 
-
-export {AppWrapper,StyledInput,Label,TextArea, Form, Section}
+export { AppWrapper, StyledInput, Label, TextArea, Form, Section, Nav, NavLinks};
