c96ad0a80e0f788a2dcffbf172d5b5dc15318563
Eduardo Lopez <ed98lopez@gmail.com>
2021-12-23 16:18:33 -0800

    added soccial media links that work

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -8,21 +8,32 @@ const SocialWrapper = styled.div`
   text-align: right;
   & a {
     margin: 0 0.25rem;
+    background: ${(props) => props.theme.colors.white};
+    
+    & svg {
+      fill: #c9c9c9;
+      transition: 0.3s ease-in-out;
+
+    }
+  }
+
+  & a:hover > svg {
+    transition: 0.3s ease-in-out;
+    fill: ${(props) => props.theme.colors.black};
   }
 `;
 
 const SocialLinks = () => {
-  const links = ["linkedIn", "instagram", "github"];
   return (
     <SocialWrapper>
       <a href="https://www.linkedin.com/in/eddie98lopez/" target="_blank">
-        <LinkedIn />
+        <LinkedIn className="svg" />
       </a>
       <a href="https://github.com/Eddie98Lopez" target="_blank">
-        <GitHub />
+        <GitHub className="svg" />
       </a>
       <a href="https://www.instagram.com/eddielopez_art/" target="_blank">
-        <Instagram />
+        <Instagram className="svg" />
       </a>
     </SocialWrapper>
   );
